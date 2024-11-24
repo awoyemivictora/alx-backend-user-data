2a260e83d9e9871d52f92506679dc9331f54d94b
Awoyemi Victor <awoyemivictora@icloud.com>
2024-11-24 04:52:29 +0100

    flask app that has a single route

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -5,11 +5,7 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def home():
-    """
-    Handle the root ("/") route and return a JSON response.
-    """
     return jsonify({"message": "Bienvenue"})
-
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
