d69c62177e9d6c44c47e7fe78d86f50244a4854a
Awoyemi Victor <awoyemivictora@icloud.com>
2024-11-24 03:13:36 +0100

    rewriting the users table by using the mapping declaration

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,15 +1,6 @@
 #!/usr/bin/env python3
-from time import timezone
-from sqlalchemy.orm import relationship, DeclarativeBase, Mapped, mapped_column
-# from app.database import Base
 from sqlalchemy.ext.declarative import declarative_base
-from datetime import date, datetime, timedelta, timezone
-from typing import List, Optional
-from sqlalchemy.sql import func
-from sqlalchemy.orm import declarative_base, mapped_column, Mapped
-from datetime import datetime
-from sqlalchemy import Enum as SAEnum, String
-from enum import Enum as PyEnum
+from sqlalchemy import Column, Integer, String
 
 Base = declarative_base()
 
@@ -18,9 +9,8 @@ class User(Base):
 
     __tablename__ = 'users'
 
-    id: Mapped[int] = mapped_column(nullable=False,
-                                    primary_key=True, unique=True)
-    email: Mapped[str] = mapped_column(nullable=False, unique=True)
-    hashed_password: Mapped[str] = mapped_column(nullable=False)
-    session_id: Mapped[str] = mapped_column(nullable=True)
-    reset_token: Mapped[str] = mapped_column(nullable=True)
+    id = Column(Integer, primary_key=True)
+    email = Column(String)
+    hashed_password = Column(String)
+    session_id = Column(String)
+    reset_token = Column(String)
