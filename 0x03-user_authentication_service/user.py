e8f702c3ba6f0d34382deb24f7bf4f75d14592b2
Awoyemi Victor <awoyemivictora@icloud.com>
2024-11-24 03:13:40 +0100

    rewriting the users table by using the mapping declaration

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -10,7 +10,7 @@ class User(Base):
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True)
-    email = Column(String)
-    hashed_password = Column(String)
-    session_id = Column(String)
-    reset_token = Column(String)
+    email = Column(String, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    session_id = Column(String, nullable=True)
+    reset_token = Column(String, nullable=True)
