b4f7a7def6ab717175651cdbe1cad7dc0e266bcd
Awoyemi Victor <awoyemivictora@icloud.com>
2024-11-24 02:43:40 +0100

    added the users table

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,26 @@
+# Every models represent a table in our database for SQLAlchemy
+from time import timezone
+from sqlalchemy import TIMESTAMP, Boolean, Column, DateTime, ForeignKey, Integer, String, Date, Text, text, Float, Enum
+from sqlalchemy.orm import relationship, DeclarativeBase, Mapped, mapped_column
+# from app.database import Base
+from sqlalchemy.ext.declarative import declarative_base
+from datetime import date, datetime, timedelta, timezone
+from typing import List, Optional
+from sqlalchemy.sql import func
+from sqlalchemy import Column, String, Boolean, Date, DateTime, ForeignKey, Text
+from sqlalchemy.orm import relationship, declarative_base, mapped_column, Mapped
+from datetime import datetime
+from sqlalchemy import Enum as SAEnum, String
+from enum import Enum as PyEnum
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'users'
+    
+    id: Mapped[int] = mapped_column(nullable=False, primary_key=True, unique=True) 
+    email: Mapped[str] = mapped_column(nullable=False, unique=True) 
+    hashed_password: Mapped[str] = mapped_column(nullable=False)
+    session_id: Mapped[str] = mapped_column(nullable=True)
+    reset_token: Mapped[str] = mapped_column(nullable=True)
+   
