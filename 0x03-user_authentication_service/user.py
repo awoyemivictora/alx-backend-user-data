6e95ec985d3498de581db836407a52e1b452a827
Awoyemi Victor <awoyemivictora@icloud.com>
2024-11-24 03:02:29 +0100

    fixing all pycodestyle errors

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -6,22 +6,21 @@ from sqlalchemy.ext.declarative import declarative_base
 from datetime import date, datetime, timedelta, timezone
 from typing import List, Optional
 from sqlalchemy.sql import func
-from sqlalchemy import Column, String, Boolean, Date, DateTime, ForeignKey, Text
-from sqlalchemy.orm import relationship, declarative_base, mapped_column, Mapped
+from sqlalchemy.orm import declarative_base, mapped_column, Mapped
 from datetime import datetime
 from sqlalchemy import Enum as SAEnum, String
 from enum import Enum as PyEnum
 
 Base = declarative_base()
 
+
 class User(Base):
+
     __tablename__ = 'users'
 
-    
-    id: Mapped[int] = mapped_column(nullable=False, primary_key=True, unique=True)
-    email: Mapped[str] = mapped_column(nullable=False, unique=True) 
+    id: Mapped[int] = mapped_column(nullable=False,
+                                    primary_key=True, unique=True)
+    email: Mapped[str] = mapped_column(nullable=False, unique=True)
     hashed_password: Mapped[str] = mapped_column(nullable=False)
     session_id: Mapped[str] = mapped_column(nullable=True)
     reset_token: Mapped[str] = mapped_column(nullable=True)
-   
-   
